20bb129b8242b04627718be38e2493a0f22033fd
Ali Rios <ali.rios@gmail.com>
2022-07-13 11:35:56 +0000

    include test with api github

--- functions_jwt.py
+++ functions_jwt.py
@@ -1,28 +1,24 @@
+from jwt import encode, decode
+from jwt import exceptions
 from datetime import datetime, timedelta
 from os import getenv
-
 from fastapi.responses import JSONResponse
-from jwt import decode, encode, exceptions
-
 
 def expire_date(days: int):
     date = datetime.now()
-    new_date = date + timedelta(days)  # numbers days
+    new_date = date + timedelta(days)
     return new_date
 
-
 def write_token(data: dict):
-    token = encode(
-        payload={**data, "exp": expire_date(2)}, key=getenv("SECRET"), algorithm="HS256"
-    )
+    token = encode(payload={**data, "exp": expire_date(2) }, key=getenv("SECRET"), algorithm="HS256")
     return token
 
 
 def validate_token(token, output=False):
     try:
         if output:
-            decode(token, key=getenv("SECRET"), algorithms=["HS256"])
-        return decode(token, key=getenv("SECRET"), algorithms=["HS256"])
+            return decode(token, key=getenv("SECRET"), algorithms=["HS256"])
+        decode(token, key=getenv("SECRET"), algorithms=["HS256"])
     except exceptions.DecodeError:
         return JSONResponse(content={"message": "Invalid Token"}, status_code=401)
     except exceptions.ExpiredSignatureError:
--- routes/auth.py
+++ routes/auth.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter
+
+from fastapi import APIRouter, Header
 from pydantic import BaseModel, EmailStr
+from functions_jwt import validate_token, write_token
+from fastapi.responses import JSONResponse
 
 auth_routes = APIRouter()
 
 class User(BaseModel):
-    username: str,
+    username: str
     email: EmailStr
-
 
 @auth_routes.post("/login")
 def login(user: User):
-    print(user)
-    return "success"
+    if user.username == "ali rios":
+        return write_token(user.dict())
+    else:
+        return JSONResponse(content={"message": "User not found"}, status_code=404)
+
+
+@auth_routes.post("/verify/token")
+def verify_token(Authorization: str = Header(default=None)):
+    token = Authorization.split(" ")[1]
+    return validate_token(token, output=True)
